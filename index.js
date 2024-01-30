5c6955bb4bbda5b9379382cdc250aec83d1621f9
Carlos Antônio <133909695+karlosantn@users.noreply.github.com>
2024-01-30 11:08:11 -0300

    base do site formulário

---
+++ index.js
@@ -0,0 +1,16 @@
+function abrirWhatsapp() {
+    var nome = document.getElementById("nome").value;
+    var telefone = document.getElementById("telefone").value;
+    var email = document.getElementById("email").value;
+    var cpf = document.getElementById("cpf").value;
+    var idade = document.getElementById("idade").value;
+    var url = "https://wa.me/+5531997657736?text="
+      + "*Inscrição Curso Desenvolvimento Web*" + "%0a"
+      + "%0a"
+      + "*Nome*: " + nome + "%0a"
+      + "*E-mail*: " + email + "%0a"
+      + "*Telefone*: " + telefone + "%0a"
+      + "*Idade*: " + idade + "%0a"
+      + "*CPF*: " + cpf + "%0a"
+    window.open(url, '_blank').focus();
+  }
